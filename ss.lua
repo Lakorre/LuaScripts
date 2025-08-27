5c83c1fa22d08add9494bc3ee8b66051f0459232
إلركس <aliahejje209@gmail.com>
2025-08-27 09:59:33 +0300

    Update ss.lua

--- ss.lua
+++ ss.lua
@@ -10,23 +10,8 @@ MachoMenuCheckbox(AliSection, "Show Player IDs",
     function() showPlayerIDs = true print("Player IDs ON") end, 
     function() showPlayerIDs = false print("Player IDs OFF") end
 )
-
--- Revive Yourself (esx) button
-MachoMenuButton(AliSection, "Revive Yourself (esx)", function()
-    TriggerEvent('esx_ambulancejob:revive', PlayerPedId())
-    MachoMenuNotification("Ali Menu", "You have been revived!")
-end)
-
--- Revive All Players (esx) button
-MachoMenuButton(AliSection, "Revive All Players (esx)", function()
-    for _, playerId in ipairs(GetActivePlayers()) do
-        local ped = GetPlayerPed(playerId)
-        TriggerEvent('esx_ambulancejob:revive', ped)
-    end
-    MachoMenuNotification("Ali Menu", "All players have been revived!")
-end)
-
 -- Close button
 MachoMenuButton(AliSection, "Close Ali", function()
     MachoMenuDestroy(AliMenu)
 end)
+
