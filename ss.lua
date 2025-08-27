6f4181d32e58c10b917bc3bd24ed679b7fed2f28
إلركس <aliahejje209@gmail.com>
2025-08-27 09:14:44 +0300

    Update ss.lua

--- ss.lua
+++ ss.lua
@@ -1,43 +1,67 @@
 -- ====== Menu Config ======
-local MenuSize = vec2(1200, 700)
-local MenuStartCoords = vec2(200, 100)
-
-local TabsBarWidth = 0
-local SectionsCount = 3 
-local SectionsPadding = 10
+local MenuSize = vec2(1600, 900)        -- تقريبًا كامل الشاشة
+local MenuStartCoords = vec2(50, 50)    -- بدء من الأعلى واليسار
+local TabsBarWidth = 200                 -- عرض الـSidebar
+local SectionsPadding = 20
 local MachoPaneGap = 10
+local SectionsCount = 3
 
 local SectionChildWidth = MenuSize.x - TabsBarWidth
 local EachSectionWidth = (SectionChildWidth - (SectionsPadding * (SectionsCount + 1))) / SectionsCount
 
 -- ====== Section coordinates ======
-local SectionOneStart = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
-local SectionOneEnd = vec2(SectionOneStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local LeftStart  = vec2(TabsBarWidth + (SectionsPadding * 1) + (EachSectionWidth * 0), SectionsPadding + MachoPaneGap)
+local LeftEnd    = vec2(LeftStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
-local SectionTwoStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
-local SectionTwoEnd = vec2(SectionTwoStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local CenterStart = vec2(TabsBarWidth + (SectionsPadding * 2) + (EachSectionWidth * 1), SectionsPadding + MachoPaneGap)
+local CenterEnd   = vec2(CenterStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
-local SectionThreeStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
-local SectionThreeEnd = vec2(SectionThreeStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
+local RightStart = vec2(TabsBarWidth + (SectionsPadding * 3) + (EachSectionWidth * 2), SectionsPadding + MachoPaneGap)
+local RightEnd   = vec2(RightStart.x + EachSectionWidth, MenuSize.y - SectionsPadding)
 
 -- ====== Create Menu Window ======
 local MenuWindow = MachoMenuWindow(MenuStartCoords.x, MenuStartCoords.y, MenuSize.x, MenuSize.y)
-MachoMenuSetAccent(MenuWindow, 255, 255, 150) -- لون أصفر فاتح
+MachoMenuSetAccent(MenuWindow, 255, 255, 150) -- أصفر فاتح
 
--- ====== First Section ======
-local FirstSection = MachoMenuGroup(MenuWindow, "Section One", SectionOneStart.x, SectionOneStart.y, SectionOneEnd.x, SectionOneEnd.y)
-MachoMenuButton(FirstSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
+local isMenuOpen = true
+
+-- ====== Sidebar ======
+local Sidebar = MachoMenuGroup(MenuWindow, "Sidebar", 10, SectionsPadding + MachoPaneGap, TabsBarWidth - 10, MenuSize.y - SectionsPadding)
+MachoMenuButton(Sidebar, "Player", function() print("[Menu] Player") end)
+MachoMenuButton(Sidebar, "Vehicle", function() print("[Menu] Vehicle") end)
+MachoMenuButton(Sidebar, "Settings", function() print("[Menu] Settings") end)
+MachoMenuButton(Sidebar, "Close", function() 
+    isMenuOpen = false 
+    MachoMenuNotification("Menu", "Closed") 
 end)
 
--- ====== Second Section ======
-local SecondSection = MachoMenuGroup(MenuWindow, "Section Two", SectionTwoStart.x, SectionTwoStart.y, SectionTwoEnd.x, SectionTwoEnd.y)
-MachoMenuButton(SecondSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
-end)
+-- ====== Left Section ======
+local LeftSection = MachoMenuGroup(MenuWindow, "Left Section", LeftStart.x, LeftStart.y, LeftEnd.x, LeftEnd.y)
+MachoMenuButton(LeftSection, "Test Button 1", function() print("Left Button 1 pressed") end)
+MachoMenuSlider(LeftSection, "Slider 1", 50, 0, 100, "%", 0, function(v) print("Slider 1: "..v) end)
 
--- ====== Third Section ======
-local ThirdSection = MachoMenuGroup(MenuWindow, "Section Three", SectionThreeStart.x, SectionThreeStart.y, SectionThreeEnd.x, SectionThreeEnd.y)
-MachoMenuButton(ThirdSection, "Close", function()
-    MachoMenuDestroy(MenuWindow)
+-- ====== Center Section ======
+local CenterSection = MachoMenuGroup(MenuWindow, "Center Section", CenterStart.x, CenterStart.y, CenterEnd.x, CenterEnd.y)
+MachoMenuButton(CenterSection, "Test Button 2", function() print("Center Button 2 pressed") end)
+MachoMenuCheckbox(CenterSection, "Checkbox 1", function() print("Checkbox ON") end, function() print("Checkbox OFF") end)
+
+-- ====== Right Section ======
+local RightSection = MachoMenuGroup(MenuWindow, "Right Section", RightStart.x, RightStart.y, RightEnd.x, RightEnd.y)
+MachoMenuButton(RightSection, "Test Button 3", function() print("Right Button 3 pressed") end)
+MachoMenuText(RightSection, "This is some text")
+
+-- ====== Page Up Toggle ======
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        if IsControlJustPressed(1, 0x21) then -- Page Up
+            if isMenuOpen then
+                isMenuOpen = false
+                MachoMenuNotification("Menu", "Hidden")
+            else
+                isMenuOpen = true
+                MachoMenuNotification("Menu", "Shown")
+            end
+        end
+    end
 end)
